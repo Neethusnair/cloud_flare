85f35ac3e189863f6f970d8695252563dcc7e7b9
Neethu <neethu.somasekharan@sreyo.com>
2021-06-22 15:52:48 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -35,14 +35,3 @@ resource "cloudflare_record" "site_a" {
   ttl     = 1
   proxied = true
 }
-
-
-# resource "cloudflare_record" "www" {
-#   zone_id = data.cloudflare_zones.domain.zones[0].id
-#   name    = "www"
-#   value   = var.site_domain
-#   type    = "CNAME"
-
-#   ttl     = 1
-#   proxied = true
-# }
